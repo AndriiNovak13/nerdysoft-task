907537fa730397f2ed62c2d182dd6ddb75b260a5
AndriiNovak13 <AndriiNovak13@users.noreply.github.com>
2020-08-21 23:38:06 +0300

    Updates

---
+++ precache-manifest.f5e866ea2bc108a6c1b3c179e7603bef.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2867c89ecc12a67dc9ee1afc796b0da7",
+    "url": "/nerdysoft-task/index.html"
+  },
+  {
+    "revision": "48ceb28c71f6e31ce8c3",
+    "url": "/nerdysoft-task/static/css/main.c5196c95.chunk.css"
+  },
+  {
+    "revision": "23b124bbbb0f1c603fc2",
+    "url": "/nerdysoft-task/static/js/2.c2f11c38.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/nerdysoft-task/static/js/2.c2f11c38.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48ceb28c71f6e31ce8c3",
+    "url": "/nerdysoft-task/static/js/main.05e0624b.chunk.js"
+  },
+  {
+    "revision": "8eb3a6a3e5b4658ef407",
+    "url": "/nerdysoft-task/static/js/runtime-main.cd504d75.js"
+  },
+  {
+    "revision": "020c97dc8e0463259c2f9df929bb0c69",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-500.020c97dc.woff2"
+  },
+  {
+    "revision": "260c8072feca314957defd2ad4ccc0ee",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-500.260c8072.eot"
+  },
+  {
+    "revision": "87284894879f5b1c229cb49c8ff6decc",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-500.87284894.woff"
+  },
+  {
+    "revision": "8c608256fb0273e3a36e6b603f71f213",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-500.8c608256.ttf"
+  },
+  {
+    "revision": "95204ac95130828753c0ee0ada537c33",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-500.95204ac9.svg"
+  },
+  {
+    "revision": "2735a3a69b509faf3577afd25bdf552e",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-700.2735a3a6.woff2"
+  },
+  {
+    "revision": "4570b93ae0caba2b938e2ddf091099ea",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-700.4570b93a.eot"
+  },
+  {
+    "revision": "57888be7f3e68a7050452ea3157cf4de",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-700.57888be7.svg"
+  },
+  {
+    "revision": "96559ffb5be917f1ce9f748bf4f34732",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-700.96559ffb.ttf"
+  },
+  {
+    "revision": "adcde98f1d584de52060ad7b16373da3",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-700.adcde98f.woff"
+  },
+  {
+    "revision": "329ae1c377b1fb667f5be6abd50327fc",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-regular.329ae1c3.ttf"
+  },
+  {
+    "revision": "479970ffb74f2117317f9d24d9e317fe",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-regular.479970ff.woff2"
+  },
+  {
+    "revision": "4be1a572fca40bcb2202504cb17aed91",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-regular.4be1a572.eot"
+  },
+  {
+    "revision": "60fa3c0614b8fb2f394fa29944c21540",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-regular.60fa3c06.woff"
+  },
+  {
+    "revision": "8681f434273fd6a267b1a16a035c5f79",
+    "url": "/nerdysoft-task/static/media/roboto-v20-latin-regular.8681f434.svg"
+  }
+]);
